3657889757d6bfdc222979000e0fcd0eb9a59796
linhaobuaa <1257819385@qq.com>
2017-03-07 20:13:39 +0800

    yuankun

--- diffusion_prediction/cron/macro_prediction/mappings_macro_prediction.py
+++ diffusion_prediction/cron/macro_prediction/mappings_macro_prediction.py
@@ -7,82 +7,86 @@ from elasticsearch.helpers import scan
 reload(sys)
 sys.path.append("../../")
 from global_utils import es_prediction as es
+from global_config import index_macro_feature_result,type_macro_feature_result
 
-def mappings_macro_task(task_name)
+def mappings_macro_prediction():
     
     index_info = {
 	'mappings':{
-	    'macro_task':{
+	    type_macro_feature_result:{
 		'properties':{
 		    'event':{
-			'type':'string',
-			'index':'not_analyzed'
+				'type':'string',
+				'index':'not_analyzed'
 		    },
 		    'topic_field':{
-			'type':'string',
-			'index':'not_analyzed'
+				'type':'string',
+				'index':'not_analyzed'
 		    },
 		    'total_num':{
-			'type':'long'
+				'type':'long'
 		    },
 		    'total_user_fans':{
 		        'type':'long'
     		    },
 		    'total_comment':{
-			'type':'long'
+				'type':'long'
 		    },
 		    'total_retweet':{
-			'type':'long'
+				'type':'long'
 		    },
 		    'total_sensitive':{
-			'type':'long'
+				'type':'long'
 		    },
 		    'total_sensitive_ratio':{
-			'type':'double'
+				'type':'double'
 		    },
 		    'total_negtive':{
-			'type':'long'
+				'type':'long'
 		    },
 		    'total_important_user':{
-			'type':'long'
+				'type':'long'
 		    },
 		    'total_origin_type':{
-			'type':'long'
+				'type':'long'
 		    },
 		    'origin_ratio':{
-			'type':'double'
+				'type':'double'
 		    },
 		    'total_retweet_type':{
-			'type':'long'
+				'type':'long'
 		    },
 		    'retweet_ratio':{
-			'type':'double'
+				'type':'double'
 		    },
 		    'total_comment_type':{
-			'type':'long'
+				'type':'long'
 		    },
 		    'comment_ratio':{
-			'type':'double'
+				'type':'double'
 		    },
 		    'at_count':{
-			'type':'string',
-			'index':'not_analyzed'
+				'type':'string',
+				'index':'not_analyzed'
 		    },
-		    'event_uid_count':{
-			'type':'long'
+		    'uid_count':{
+				'type':'long'
 		    },
 		    'event_trend_delta':{
-			'type':'string',
-			'index':'not_analyzed'
+				'type':'string',
+				'index':'not_analyzed'
 		    },
-		    'predict_value':{
-			'type':'long'
+		    'predict_weibo_value':{
+				'type':'long'
+		    },
+		    'predict_user_value':{
+				'type':'long'
 		    },
 		    'predict_rank':{
-			'type':'double'
+				'type':'double'
 		    },
 		    'update_time':{
-			'type':'long'
+				'type':'long'
 		    }		    
 		}
 	    }
@@ -90,17 +94,17 @@ def mappings_macro_task(task_name)
 
     }
 
-    index_name = 'macro_prediction_' + task_name
+    #index_name = 'macro_prediction_' + task_name
 
-    if not es.indices.exists(index=index_name):
+    if not es.indices.exists(index=index_macro_feature_result):
 
-        es.indices.create(index=index_name, body=index_info, ignore=400)
+        es.indices.create(index=index_macro_feature_result, body=index_info, ignore=400)
 
     return "1"
 
 
 if __name__ == '__main__':
 
-	task_name = 'macro_prediction_'+'mao'
-	mappings_macro_prediction(task_name)
+	#task_name = 'macro_prediction_'+'mao'
+	mappings_macro_prediction()
     
